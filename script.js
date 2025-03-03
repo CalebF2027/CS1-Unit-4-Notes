1dcdf19db7164739f9a998c9622922c25a4ab483
caleb <caleb.fisher2027@gbwl.org>
2025-03-03 17:11:44 +0000

    finsihed first note

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+/* This is a MULTI-LINE comment in JS
+Below is an example of a JS STATEMENT
+(instruction/command)
+Statements end with a SEMICOLON (;)
+*/
+console.log("welcome to JavaScript!!!");
+
+// This is a SINGLE-LINE comment
+// console.log is a FUNCTION to display information
+console.log("this is the console/output")
+
+// DECLARE a VARIABLE to hold data
+// use the LET keyword to create the variable
+let pokemon;
+
+// ASSIGN a VALUE to that variable
+// like packing an item into a box
+pokemon = "Pikachu" ;
+
+// Can DECLARE + ASSIGN in one statement
+let name = "Caleb" ;
